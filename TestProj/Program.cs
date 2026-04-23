3b96ab8b4c62f84456b6bb2af9f6208c1a82aa09
maksluczak <maksluczak99@gmail.com>
2026-04-23 20:10:29 +0000

    delete test directory

--- TestProj/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Genocs.QRCodeGenerator.Encoder;
-
-var generator = new QRCodeGenerator();
-var data = generator.CreateQrCode("https://example.com", QRCodeGenerator.ECCLevel.Q);
-
-var qr = new BitmapByteQRCode(data);
-byte[] bmpBytes = qr.GetGraphic(5);
-
-File.WriteAllBytes("qrcode.bmp", bmpBytes);
